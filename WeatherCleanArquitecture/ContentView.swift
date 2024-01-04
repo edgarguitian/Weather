8ef815bbca0073e34d8243b42a82becc28e59d3c
Edgar <edguitian@imagine800.es>
2024-01-04 17:40:43 +0100

    Initial Commit

---
+++ WeatherCleanArquitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherCleanArquitecture
+//
+//  Created by Edgar Guitian Rey on 4/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherCleanArquitecture/WeatherCleanArquitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCleanArquitectureApp.swift
+//  WeatherCleanArquitecture
+//
+//  Created by Edgar Guitian Rey on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCleanArquitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
